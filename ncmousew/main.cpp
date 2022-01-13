c31898b6f7b8dd383a948bd89cdb8146908bac77
Tiit Ülejõe <tiit@infokiir.ee>
2022-01-13 17:12:54 +0200

    järjekorda muudetud

--- ncmousew/main.cpp
+++ ncmousew/main.cpp
@@ -86,12 +86,15 @@ int main()
 	bool result_calculated = false;
 	const auto init_r = init_screen_mouse_keyb();
 
-	draw_keyboard_grid(keyb_starty, 0, keyb_starty + keyb_grid_height, keyb_grid_width, cols, rows);
-	fill_keyboard_grid();
+
 
 	debug_win = newwin(debug_win_height, debug_win_width, 1, debug_win_startx);
 	computer c(debug_win, event);
 	interface ui(c);
+
+	draw_keyboard_grid(keyb_starty, 0, keyb_starty + keyb_grid_height, keyb_grid_width, cols, rows);
+	fill_keyboard_grid();
+
 	rectangle_around_window(debug_win_height, debug_win_width, 1, debug_win_startx, "DEBUG");
 	WINDOW* calc_win = newwin(calc_win_height, calc_win_width, 1, 5);
 	rectangle_around_window(calc_win_height, calc_win_width, 1, 5, "CALC");
